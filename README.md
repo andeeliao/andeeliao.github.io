51d2049f791d7a9b288b5d2b12cb220b64563ac5
Andee Liao <xueqi.liao@gmail.com>
2026-02-27 19:09:34 -0500

    fix folders

--- README.md
+++
@@ -1,2 +0,0 @@
-# rackethub
-phil's website
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Announcing Summer Camps!",
+        "excerpt":"Summer camp description blah blah  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      }]
